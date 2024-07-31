8719c21b9cb0275925388ac04ff54b72ccad7f92
Gabrielle P. <gpoerwawinata@gmail.com>
2024-07-31 14:56:37 +0200

    Create LICENSE
    adding MIT license

--- app_annotate_document.py
+++ app_annotate_document.py
@@ -1,3 +1,27 @@
+"""
+MIT License
+
+Copyright (c) 2024 Clara Rus and Gabrielle Poerwawinata
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
 import argparse
 import json
 
@@ -8,6 +32,17 @@ from flask import Flask, render_template, request, redirect, session, flash, mak
 from flask_login import login_user, LoginManager, logout_user, current_user
 from mongoengine import *
 
+from config import Config
+import os
+import logging
+from src.utils import add_fields_from_data
+import argparse
+import json
+import numpy as np
+import database
+from flask import Flask, render_template, request, redirect, session, flash, make_response, jsonify
+from flask_login import login_user, LoginManager, logout_user, current_user
+from mongoengine import *
 from config import Config
 import os
 import logging
