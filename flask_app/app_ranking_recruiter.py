c72fc2f353fb92de111d5654cd8983473f2d810b
ElyanneOey <99719450+ElyanneOey@users.noreply.github.com>
2025-04-23 13:34:48 +0200

    recruiter expansion html code

--- flask_app/app_ranking_recruiter.py
+++ flask_app/app_ranking_recruiter.py
@@ -253,7 +253,7 @@ def index_ranking(experiment_id, n_task, doc_id):
     if doc_id != 'view':
         doc_obj = docs_obj[int(doc_id) - 1]
 
-        return render_template('doc_ranking_view_information_template_recruiter.html', doc_obj=doc_obj,
+        return render_template('doc_ranking_view_plot_and_discription_template_recruiter.html', doc_obj=doc_obj,
                                field_names=doc_field_names_view, doc_index=doc_id, task_description=task_description)
 
     user = database.User.objects(_user_id=session['user_id']).first()
