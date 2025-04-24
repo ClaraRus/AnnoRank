2bbc9350ca2990490829f651b34637fb49fd5651
clararus <clararus98@gmail.com>
2025-04-24 12:57:33 +0200

    fix bug on view template

--- flask_app/static/js_scripts/viewDocInfo.js
+++ flask_app/static/js_scripts/viewDocInfo.js
@@ -5,15 +5,19 @@ function loadAndToggleVisibility(targetElementId,docId, htmlFile) {
     const targetElement = document.getElementById(targetElementId);
     const container = targetElement.querySelector("#injected-container");
 
-    console.log(targetElement)
-    console.log(container)
+
     if (targetElement.style.display === 'none' || targetElement.style.display === '') {
         fetch(htmlFile)
             .then(response => response.text())
             .then(data => {
-                console.log(data)
-                console.log(targetElement)
-                container.innerHTML = data;
+                // TO DO: update the app_ranking to work under the same logic as the XAI UI
+                if (!container) {
+                    // for the app_ranking.py
+                    targetElement.innerHTML = data;
+                }else{
+                    // for the exntensions with the XAI UI
+                    container.innerHTML = data;
+                }
 
                 targetElement.style.display = 'table-row';
                 // targetElement.style.display = '';
