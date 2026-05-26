fb8f7cd4ac654cb3ceeef922d8d220712ea5fe91
ElyanneOey <99719450+ElyanneOey@users.noreply.github.com>
2026-05-26 21:14:54 +0200

    added findhr name to select dataset names

--- run_apps.sh
+++ run_apps.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-read -p "Please select dataset name cvs, xing, flickr or amazon: " dataset_name
+read -p "Please select dataset name cvs, xing, flickr, amazon or findhr: " dataset_name
 sed -i~ "s/^DATANAME=.*/DATANAME=$dataset_name/" .env
 docker compose up
